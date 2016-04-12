3f6af2138fa0cf616bcadaf93e77f6284fb05447
XBZX <liwei429>
2016-04-12 18:43:09 +0800

    4.12.02

--- test/a.py
+++ test/a.py
@@ -1,7 +1,23 @@
 # -*- coding: utf-8 -*-
 #! /usr/bin/env python
-import os
-
-os.system("curl http://192.168.3.206:6800/schedule.json -d project=zbzxtest -d spider=12312")
-# os.system("ipconfig")
-
+import urllib2
+# import urllib
+# url= "http://www.ccgp.gov.cn/json/bid"
+# req = urllib2.Request(url)
+# data={"bid_time":"0",
+#     "bid_type":"",
+#     "method":"areaBid",
+#     "pagenum":"1",
+#     "pagesize":"13",
+#     "pin_mu":"",
+#     "zone_id":"610000"}
+# data = urllib.urlencode(data)
+# opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
+#
+# html = opener.open(req,data)
+#
+#
+# print html.read()
+#
+#
+print urllib2.urlopen("http://www.ccgp.gov.cn/cggg/zygg/index.htm").read()
